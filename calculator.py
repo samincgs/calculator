5ea8c41adc53432ac8d46ea72946d0a998971469
samin <samincgs@gmail.com>
2024-07-28 15:35:08 -0400

    starting to create labels for the calculator (for the formula and result)

--- calculator.py
+++ calculator.py
@@ -11,11 +11,28 @@ class Calculator(ctk.CTk):
         self.iconbitmap('empty.ico')
         self.resizable(False, False)
         
+        #layout
+        rows = list(range(MAIN_ROWS))
+        cols = list(range(MAIN_COLS))
+        self.rowconfigure(rows, weight=1, uniform='a')
+        self.grid_columnconfigure(cols, weight=1, uniform='a')
+        
+        # widgets
+        self.create_widgets()
         
         # run
         self.mainloop()
 
+    def create_widgets(self):
+        #output labels
+        OutputLabel(self, 0, 'se') # formula
+        OutputLabel(self, 1, 'e') # result
 
+class OutputLabel(ctk.CTkLabel):
+    def __init__(self, parent, row, anchor):
+        super().__init__(master=parent, text='123')
+        self.grid(row = row, column = 0, columnspan = 4, sticky = anchor, padx = 10)
+        
 
 if __name__ == '__main__':
     is_dark = darkdetect.isDark()
