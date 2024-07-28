07c2e81ae8a1ce5aa0eec3c801145f977264eddb
samin <samincgs@gmail.com>
2024-07-28 14:49:08 -0400

    created a default customtkinter app

--- calculator.py
+++ calculator.py
@@ -1,1 +1,22 @@
 import customtkinter as ctk
+import darkdetect
+from settings import *
+
+class Calculator(ctk.CTk):
+    def __init__(self, is_dark):
+        super().__init__(fg_color=(WHITE, BLACK))
+        ctk.set_appearance_mode('dark' if is_dark else 'light')
+        self.title('Calculator')
+        self.geometry(f'{WINDOW_SIZE[0]}x{WINDOW_SIZE[1]}')
+        self.iconbitmap('empty.ico')
+        self.resizable(False, False)
+        
+        
+        # run
+        self.mainloop()
+
+
+
+if __name__ == '__main__':
+    is_dark = darkdetect.isDark()
+    Calculator(is_dark)
---
+++ settings.py
@@ -0,0 +1,24 @@
+# size
+WINDOW_SIZE = (400, 700)
+MAIN_ROWS = 7
+MAIN_COLS = 4
+
+#text
+FONT = 'Helvetica'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# colors
+BLACK = '#000000'
+WHITE = '#EEEEEE'
